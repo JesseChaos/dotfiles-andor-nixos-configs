e55352a8d925ffd13ebf1ee10dd51ba275244d5e
JesseChaos <jessecvaughn@gmail.com>
2018-07-11 22:16:52 +0000

    Cleaned and expanded
    added virtualization stuff (docker, vbox), redshift, re-organized a few sections, added a window manager (xmodal). probably more

--- configuration.nix
+++ configuration.nix
@@ -1,70 +1,69 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# Micro graphical configuration that gets you started with a gnome3 (+ optionally xmonad) desktop
+# * Note that gnome3 installs a *lot* of stuff from the internet
+# * After modifying copying this configuration, run:
+#   $ nixos-rebuild switch # sometimes it helps to try again
+#                          # if this step fails for some reason
+#   $ reboot
 
 { config, pkgs, ... }:
 
 {
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
   # Enable automatic updating of the system
   system.autoUpgrade.enable = true;
 
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      # <unstable/nixos/modules/services/desktops/gnome3/chrome-gnome-shell.nix>
-    ];
-
-  # Disable the module from default channel
-  # disabledModules = [ "desktops/gnome3/chrome-gnome-shell.nix" ];
-
-  # Boot settings
+  # Boot settings.
   boot = {
     initrd = {
-	# Disable journaling check as vbox doesn't need it
+      # Disable journaling check on boot because virtualbox doesn't need it
       checkJournalingFS = false;
-      	# make it pretty
-      kernelModules = [ " fbcon" ];
+      kernelModules = ["fbcon"];
+    };
+
+    # Use boot splash
+    plymouth.enable = true;
+
+    # Use the GRUB 2 boot loader.
+    loader.grub = {
+      enable = true;
+      version = 2;
+      # Define on which hard drive you want to install Grub.
+      device = "/dev/sda";
     };
   };
-
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
-
-  # networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Select internationalisation properties.
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
+  
+  # Enable networking.
+  networking = {
+    hostName = "NixOS-OnBL"; # Define your hostname.
+    # hostId = # (use whatever was generated) 
+    # wireless.enable = true;  # not needed with virtualbox
   };
 
   # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "America/New_York";
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
 
-  environment.systemPackages = [ pkgs.google-chrome ];
+  environment.etc.current-nixos-config.source = ./.;
+  # Currently broken for some reason
+  # environment.variables.NIX_AUTO_RUN
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
 
     #Core
-    wget vim
+    wget
     tree
     nano
 
     #General
     google-chrome
     docker
+		android-udev-rules jmtpfs
 
     #Code
     git
@@ -76,87 +75,91 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.bash.enableCompletion = true;
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
-  # List services that you want to enable:
+  # List services that you want to enable.
   services = {
-
-    gnome3 = {
-      chrome-gnome-shell.enable = true;
-      gnome-keyring.enable = true;
-    };
- 
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+    # Enable the OpenSSH daemon.
     openssh.enable = true;
+    
+    # Enable CUPS to print documents.
+    # services.printing.enable = true;
+    
+    # Enable Gnome keyring
+    gnome3.gnome-keyring.enable = true;
+    # Enable Gnome integrations
+    gnome3.chrome-gnome-shell.enable = true;
 
+    
+    # Enable the X11 windowing system.
     xserver = {
       enable = true;
-      libinput.enable = true;
-
-      # Gnome 3 works better with GDM
+      layout = "us";
+      # Touchpad support
+      # libinput.enable = true;
+      
+      # Gnome desktop
+      # * Slightly more familiar than KDE for people who are used to working with Ubuntu
+      # * Gnome3 works out of the box with xmonad
+      # * Gnome 3 works better with GDM and disable KDE display manager
       displayManager.gdm.enable = true;
-
-      # Enable Gnome 3
-      desktopManager.gnome3.enable = true;
-
-      #Disable KDE
       displayManager.sddm.enable = false;
-      desktopManager.plasma5.enable = false;
-
-      #Use closed source NVidia drivers vs nouvaeu
-      videoDrivers = ["nvidia"];
+      
+      desktopManager = {
+        gnome3.enable = true;
+        default = "gnome3";
+                
+        # Disable KDE desktop manager
+        plasma5.enable = false;
+      };
+      
+      # Enable XMonad Desktop Environment. (Optional)
+      windowManager = {
+        xmonad.enable = true; 
+        xmonad.enableContribAndExtras = true;
+      };
+      			
+      # Use closed source NVidia drivers vs nouvaeu
+      # videoDrivers = ["nvidia"];
+    };  
+    # Redshift light adjustment
+    redshift = { 
+      enable = true;
+      
+      # Round Hill, VA
+      latitude = "39.13";
+      longitude = "77.78";
+    };
+   
+    # Not sure what this is
+    # unclutter.enable = true;
+  };
+    
+  # Virtualization stuff
+  virtualization = {
+    
+    # Virtualbox
+    # virtualbox.host = {
+      # enable = true;
+      # enableHardening = false;
+      # addNetworkInterface = true;
+    # };
+    
+    # Docker
+    docker = {
+      enable = true;
+      storageDriver = "devicemapper";
     };
   };
 
   # List hardware tweaks
   # hardware.cpu.intel.updateMicrocode = true;
   hardware.opengl.driSupport32Bit = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
+  
   # Enable sound.
   # sound.enable = true;
   # hardware.pulseaudio.enable = true;
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable touchpad support.
-  # services.xserver.libinput.enable = true;
-
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.extraUsers.Jesse = {
-    isNormalUser = true;
-    uid = 1000;
-    group = "users";
-    extraGroups = [ "wheel" "networkmanager" "docker"];
-    password = "demo";
-  };
-
-  # Set root password
-  users.users.root.initialHashedPassword = "";
-  users.users.root.password = "demo";
-
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "18.03"; # Did you read the comment?
-
+  
+  # The NixOS release to be compatible with for stateful data such as databases.
+  # system.stateversion = "18.03";
+  
 }
